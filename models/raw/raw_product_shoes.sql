d5263befe447513077a78c370d7f7fa338f4d314
Altheo Saquilayan <altheo.saqui@gmail.com>
2025-05-25 02:23:29 +0000

    Update master

--- models/raw/raw_product_shoes.sql
+++ models/raw/raw_product_shoes.sql
@@ -1,18 +1,26 @@
-{{ config(materialized='view') }}
+{{  
+  config(
+    materialized = 'view',
+    alias        = 'raw_product_shoes'
+  )  
+}}
 
-select
-  $1:id::string      as id,
-  $1:title::string   as title,
-  $1:subTitle::string as subTitle,
-  $1:url::string     as url,
-  $1:image::string   as image,
-  $1:price_sale::float as price_sale,
-  $1:price_original::float as price_original,
-  $1:gender::array     as gender,
-  $1:age_group::string as age_group,
-  $1:brand::string     as brand,
-  object_delete(
-    $1,
-    'id','title','subTitle','url','image','price_sale','price_original','gender','age_group','brand'
-  )::variant as extra
-from @raw_product_shoes_stage (file_format => 'csv_raw_format') t;
+with base as (
+
+  select
+    id,
+    title,
+    subTitle,
+    url,
+    image,
+    price_sale,
+    price_original,
+    gender,
+    age_group,
+    brand,
+    extra
+  from {{ source('raw', 'RAW_PRODUCT_SHOES_RAW') }}
+
+)
+
+select * from base
