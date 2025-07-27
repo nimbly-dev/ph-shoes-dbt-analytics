a54195aa7c93f29047093d89e807d378b93db81f
Altheo Saquilayan <altheo.saqui@gmail.com>
2025-07-27 06:08:57 +0000

    Test23

--- models/marts/product_shoes/fact/fact_product_shoes.sql
+++ models/marts/product_shoes/fact/fact_product_shoes.sql
@@ -8,6 +8,56 @@
 
 WITH raw_data AS (
     SELECT
+      id,
+      title,
+      subtitle,
+      url,
+      image,
+      price_sale,
+      price_original,
+      gender,
+      age_group,
+      brand,
+      dwid,
+      year,
+      month,
+      day,
+      extra
+    FROM {{ ref('stg_product_shoes') }}
+    -- no manual date filter: MERGE on (id, dwid) ensures only new batches get upserted
+),
+
+deduped AS (
+    SELECT
+      *,
+      ROW_NUMBER() OVER (PARTITION BY id, dwid ORDER BY id) AS row_num
+    FROM raw_data
+),
+
+final AS (
+    SELECT
+      brand,
+      dwid, year, month, day,
+      id, title, subtitle, url, image,
+      price_sale, price_original,
+      gender, age_group,
+      extra
+    FROM deduped
+    WHERE row_num = 1
+)
+
+SELECT * 
+FROM final
+{{  
+  config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key=['id','dwid']
+  ) 
+}}
+
+WITH raw_data AS (
+    SELECT
         id,
         title,
         subtitle,
